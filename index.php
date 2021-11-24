e1c82c4079b9ad6acb3b05a276155da61361167e
carsd3 <littleguitar2001@gmail.com>
2021-11-24 15:57:54 +0100

    a

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang = 'en'>
+    <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="styles/index.css" />
+        <title>Hello World!</title>
+    </head>
+    <body>
+        <div id="head">
+            <a class = "link" href="index.html">
+                <img class = "start" src = "img/icon.jpg" alt = "test">
+            </a>
+            <h1 class = "title">Hello World!</h1>
+            <div class="tab">
+                <a id = "tabs" href = "common/discounts.html">
+                    <h2>Discounts</h2>
+                </a>
+                <a id = "tabs" href = "common/products.html">
+                    <h2>Products</h2>
+                </a>
+                <a id = "tabs" href = "common/about.html">
+                    <h2>About</h2>
+                </a>
+                <a id = "tabs" href = "common/contact_us.html">
+                    <h2>Contact Us</h2>
+                </a>
+                <a id = "tabs" href = "common/account.html">
+                    <h2>Account</h2>
+                </a>
+            </div>
+            <div class="searchbar">
+                <form action="common/products.html" method="post">
+                    <input type="text" name="search">
+                    <input type="submit">
+                </form>
+            </div>
+        </div>
+
+        <hr>
+
+
+    </body>
+</html>
